3f8dddcc31b55820e527568e94a1cfb62f9b5719
Mba <sundaymba91@gmail.com>
2020-12-03 04:02:26 +0100

    Added gzip compression

--- Backup_with_compression.sh
+++ Backup_with_compression.sh
@@ -18,22 +18,22 @@ mkdir -p backups #Create the backups folder if it does not exist
 backupLocation="./backups"
 
 time="$(date +'%d_%m_%Y_%H_%M_%S')"
-backupFileName="database_backup_$time".sql #Name to store the database backUp
+backupFileName="database_backup_$time".sql.gz #Name to store the database backUp
 backupfileLocation="$backupLocation/$backupFileName"
 
 operationLogs="$backupLocation/"db_backup_log_"$(date +'%Y_%m')".txt
 echo "mysqldump started at $(date +'%d-%m-%Y %H:%M:%S') ," >> "$operationLogs"
 
-mysqldump --user=$oldDB_user --password=$oldDB_password -h $db_host -P $db_port --default-character-set=utf8 $oldDB_database > "$backupfileLocation"
+mysqldump --user=$oldDB_user --password=$oldDB_password -h $db_host -P $db_port --default-character-set=utf8 $oldDB_database | gzip > "$backupfileLocation"
 
 echo "mysqldump finished at $(date +'%d-%m-%Y %H:%M:%S')" >> "$operationLogs"
-echo "file permission changed" >> "$operationLogs"
-echo "DB import starting"
-
-mysql --user=$newDB_user --password=$newDB_password -h $db_host -P $db_port $newDB_database  < $backupfileLocation
-echo "DB import complete"
-find "$backupLocation" -name db_backup_log_* -mtime +8 -exec rm {} \;
-echo "old files deleted" >> "$operationLogs"
+echo "DB import starting" >> "$operationLogs"
+#UNZIP THE FILE
+tmp_sql_file_name="sql_db_backup_$time".sql
+gunzip -c $backupfileLocation > $backupLocation/$tmp_sql_file_name
+mysql --user=$newDB_user --password=$newDB_password -h $db_host -P $db_port $newDB_database  < $backupLocation/$tmp_sql_file_name 
+unlink $backupLocation/$tmp_sql_file_name && unlink $backupfileLocation #DELETE THE DECOMPRESSED SQL FILE
+echo "DB import complete and files deleted">> "$operationLogs"
 echo "operation finished at $(date +'%d-%m-%Y %H:%M:%S')" >> "$operationLogs"
 echo "*****************" >> "$operationLogs"
 exit 0
